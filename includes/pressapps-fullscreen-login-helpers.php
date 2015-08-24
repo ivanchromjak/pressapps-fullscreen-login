03d127600fc21d5d6c1cb0969f552c5c89182d3b
Champ Camba <champ.kazban25@gmail.com>
2015-08-24 14:00:47 +0800

    Add helper functions file

---
+++ includes/pressapps-fullscreen-login-helpers.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Helpers functions
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+
+
+/**
+ * Add login/logout/register link
+ * @param Array $atts    
+ *
+ * @since    1.0.0
+ */
+if( ! function_exists('pafl_link_text') ){
+	function pafl_link_text( $atts ){
+
+		$atts = shortcode_atts(
+			array(
+				'login_text' 	=> 'Login',
+				'logout_text' 	=> __('Logout','pressapps'),
+				'register'		=> false,
+				'register_text' => __('Create an Account','pressapps')
+			), $atts, 'pafl_link' 
+		);
+
+		if( $atts['register'] ){
+		    echo "<a href='javascript:;'  data-form='register'  title='pafl-trigger-overlay'>".$atts['register_text']."</a>";
+		}else{
+			if( is_user_logged_in() ){
+			    echo "<a href='".wp_logout_url()."' >".$atts['logout_text']."</a>";
+			}else{
+			    echo "<a href='javascript:;'  data-form='login'  title='pafl-trigger-overlay'>".$atts['login_text']."</a>";
+			}
+		}
+	}
+}
